a838cfa63eb44aff2c4f3f26d633695f8eabc750
mpkato <kato@dl.kuis.kyoto-u.ac.jp>
2016-10-10 11:52:14 +0900

    Add setup_requires to setup.py (#32)

--- setup.py
+++ setup.py
@@ -1,31 +1,21 @@
 # -*- coding:utf-8 -*-
 from setuptools import setup
-from setuptools.command.test import test as TestCommand
-
-class PyTest(TestCommand):
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tests(self):
-        import pytest
-        pytest.main(self.test_args)
 
 setup(
     name = "interleaving",
     packages = ["interleaving", "interleaving.simulation"],
     version = "0.0.1",
     description = "Interleaving library for ranking evaluation",
-    author = "Makoto P. Kato",
+    author = "Makoto P. Kato, Tomohiro Manabe",
     author_email = "kato@dl.kuis.kyoto-u.ac.jp",
     license     = "MIT License",
     url = "https://github.com/mpkato/interleaving",
-    entry_points='',
+    setup_requires = [
+        'numpy'
+    ],
     install_requires = [
         'numpy',
         'scipy'
     ],
     tests_require=['pytest'],
-    cmdclass = {'test': PyTest}
 )
