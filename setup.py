89c4863322bcfcb31242ee5123f4ed42bd7ce7f4
mpkato <kato@dl.kuis.kyoto-u.ac.jp>
2016-10-10 12:24:41 +0900

    Add test command to setup.py again

--- setup.py
+++ setup.py
@@ -1,5 +1,16 @@
 # -*- coding:utf-8 -*-
 from setuptools import setup
+from setuptools.command.test import test as TestCommand
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        pytest.main(self.test_args)
 
 setup(
     name = "interleaving",
@@ -18,4 +29,5 @@ setup(
         'scipy'
     ],
     tests_require=['pytest'],
+    cmdclass = {'test': PyTest}
 )
