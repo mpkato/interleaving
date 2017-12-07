1c8ba33ebb2f722ceb3168970d24a5a8dce5b564
mpkato <kato@dl.kuis.kyoto-u.ac.jp>
2017-12-07 22:49:04 +0900

    Improve tests for simulation

--- tests/test_simulation.py
+++ tests/test_simulation.py
@@ -1,3 +1,6 @@
+import os
+import pytest
+import math
 import interleaving as il
 import numpy as np
 from collections import defaultdict
@@ -5,49 +8,48 @@ np.random.seed(0)
 
 class TestSimulation(object):
 
-    def test_simulator(self):
-        sim = il.simulation.Simulator("./MQ2008/Fold1/train.txt", 1)
-
-        bm25 = il.simulation.Ranker(lambda x: x[25])
-        lm = il.simulation.Ranker(lambda x: x[40])
-        pr = il.simulation.Ranker(lambda x: x[41])
-        rankers = [bm25, lm, pr]
-
+    def test_simulator_evaluate(self, data_filepath):
+        sim = il.simulation.Simulator(data_filepath, 10)
+        m1 = il.simulation.Ranker(lambda x: x[1])
+        m2 = il.simulation.Ranker(lambda x: x[2])
+        m3 = il.simulation.Ranker(lambda x: x[3])
+        rankers = [m1, m2, m3]
         user = il.simulation.User(click_probs=[0.0, 0.5, 1.0],
             stop_probs=[0.0, 0.0, 0.0])
-
         res = sim.evaluate(rankers, user, il.TeamDraft)
+        assert len(res) == 10
         result = defaultdict(int)
-        for rs in res:
-            for r in rs:
-                result[r] += 1
-        assert result[(1, 0)] > result[(0, 1)]
-        assert result[(1, 2)] > result[(2, 1)]
+        for r in res:
+            for (i, j) in r:
+                result[(i, j)] += 1
+        assert result[(0, 1)] > result[(1, 0)]
         assert result[(0, 2)] > result[(2, 0)]
+        assert result[(1, 2)] > result[(2, 1)]
 
-    def test_simulator_ndcg(self):
-        sim = il.simulation.Simulator("./MQ2008/Fold1/train.txt", 1)
+    def test_simulator_ndcg(self, data_filepath):
+        sim = il.simulation.Simulator(data_filepath, 1)
 
-        bm25 = il.simulation.Ranker(lambda x: x[25])
-        lm = il.simulation.Ranker(lambda x: x[40])
-        pr = il.simulation.Ranker(lambda x: x[41])
-        rankers = [bm25, lm, pr]
+        m1 = il.simulation.Ranker(lambda x: x[1])
+        m2 = il.simulation.Ranker(lambda x: x[2])
+        m3 = il.simulation.Ranker(lambda x: x[3])
+        rankers = [m1, m2, m3]
 
-        ndcgs = sim.ndcg(rankers, 10)
+        ndcgs = sim.ndcg(rankers, 2)
+        assert ndcgs[0] == 1
+        ndcg_1 = (1.0 + 2.0 / np.log2(3.0)) / (2.0 + 1.0 / np.log2(3.0))
+        assert np.abs(ndcgs[1] - ndcg_1) < 10e-10
+        ndcg_2 = (1.0 / np.log2(3.0)) / (2.0 + 1.0 / np.log2(3.0))
+        assert np.abs(ndcgs[2] - ndcg_2) < 10e-10
 
-    def test_measure_error(self):
-        sim = il.simulation.Simulator("./MQ2008/Fold1/train.txt", 10)
+    def test_measure_error(self, data_filepath):
+        # 2 > 1 > 0
+        il_result = [[(0, 2)], [(2, 0), (2, 1)], [(2, 0), (1, 0)]]
+        ndcg_result = {0: 0.2, 1: 0.1, 2: 0.3}
 
-        tfidf = il.simulation.Ranker(lambda x: x[15])
-        bm25 = il.simulation.Ranker(lambda x: x[25])
-        lm = il.simulation.Ranker(lambda x: x[40])
-        pr = il.simulation.Ranker(lambda x: x[41])
-        rankers = [tfidf, bm25, lm, pr]
+        error = il.simulation.Simulator.measure_error(il_result, ndcg_result)
+        assert error == 2 / 6
 
-        user = il.simulation.User(click_probs=[0.4, 0.7, 0.9],
-            stop_probs=[0.1, 0.3, 0.5])
-
-        ndcg_result = sim.ndcg(rankers, 10)
-
-        il_result = sim.evaluate(rankers, user, il.TeamDraft)
-        error = sim.measure_error(il_result, ndcg_result)
+    @pytest.fixture
+    def data_filepath(self):
+        return os.path.join(os.path.dirname(os.path.abspath(__file__)),
+            "fixtures", "l2r_sample.txt")
