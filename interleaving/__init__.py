80deab9b8fcc412cc013c478db64a6235839d5d2
mpkato <kato@dl.kuis.kyoto-u.ac.jp>
2018-09-16 00:01:05 +0900

    Import PairwisePreference and PairwisePreferenceRanking #57

--- interleaving/__init__.py
+++ interleaving/__init__.py
@@ -2,11 +2,13 @@ from .ranking import BalancedRanking
 from .ranking import CreditRanking
 from .ranking import ProbabilisticRanking
 from .ranking import TeamRanking
+from .ranking import PairwisePreferenceRanking
 
 from .balanced import Balanced
 from .probabilistic import Probabilistic
 from .team_draft import TeamDraft
 from .optimized import Optimized
 from .roughly_optimized import RoughlyOptimized
+from .pairwise_preference import PairwisePreference
 
 from . import simulation
